7f4d56e1330f7a379eb68d2ca1ae323b92ffbfc0
juanpiml2@hotmail.com <juanpiml2@hotmail.com>
2020-03-30 15:05:09 -0400

    Probando la nueva rama.

---
+++ Final_progIII/Final_progIII/Models/Pacientes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Final_progIII.Models
+{
+    public class Pacientes
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Cedula { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(100)]
+        public bool Asegurado { get; set; }
+
+
+    }
+}
---
+++ Final_progIII/Final_progIII/Models/Prueba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final_progIII.Models
+{
+    public class Prueba
+    {
+
+
+
+    }
+}
